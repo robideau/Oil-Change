97c7ee0be9892cebab29f5c635aaa175d2beb9dd
David Robideau <robideau@iastate.edu>
2016-02-03 17:39:42 -0600

    Added car debug scene, wrote skeleton for PlayerCarController script
    Next step will be to add wheel rotation and physics.

---
+++ Assets/Scripts/PlayerCar/PlayerCarController.cs
@@ -0,0 +1,53 @@
+﻿/* David Robideau
+ * 2/3/2016
+ * 
+ * This controller handles player input and the player car's motion and physics.
+ *
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class PlayerCarController : MonoBehaviour {
+
+	public GameObject carBody;
+	public GameObject carWheels;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+
+		if (Input.GetKeyDown ("w")) {
+			foreach (Transform wheel in carWheels.transform) {
+				//Rotate wheel forward
+			}
+		}
+
+		if (Input.GetKeyDown ("s")) {
+			foreach (Transform wheel in carWheels.transform) {
+				//Rotate wheel backwards
+			}
+		}
+
+		if (Input.GetKeyDown ("d")) {
+			foreach (Transform wheel in carWheels.transform) {
+				//Isolate front wheels - revise later to use tags?
+				if (wheel.gameObject.name.Contains ("Front")) {
+					//Turn front wheels right
+				}
+			}
+		}
+
+		if (Input.GetKeyDown ("d")) {
+			foreach (Transform wheel in carWheels.transform) {
+				//Isolate front wheels - revise later to use tags?
+				if (wheel.gameObject.name.Contains ("Front")) {
+					//Turn front wheels left
+				}
+			}
+		}
+
+	}
+}
