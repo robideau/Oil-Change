339feeabc5352728972062c1ca6ad6a7e01e04ad
Nick Bramanti <bramanti@iastate.edu>
2016-02-09 20:27:45 -0600

    set up files for games and servers

---
+++ Assets/Server/ServerNetworkManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServerNetworkManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void startServer() {
+
+	}
+
+	void stopServer() {
+
+	}
+}
