850f76eb326b0491e0f068b01a30ddfb000c1365
Nick Bramanti <bramanti@iastate.edu>
2016-02-09 20:52:46 -0600

    done working 2/9, wrote pseudocode for intializing server and stopping server, plan on doing player connection next

--- Assets/Server/ServerNetworkManager.cs
+++ Assets/Server/ServerNetworkManager.cs
@@ -3,21 +3,31 @@ using System.Collections;
 
 public class ServerNetworkManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	//the maximum number of clients allowed on the server
+	public int maxClients;
+	//port number to be listening to
+	public int listenPort;
+	//if Network Address Translation is on or not
+	//need to read up on NAT and NAT algorithm
+	//allows for mapping multiple addresses to a single destination router
+	public bool NATon;
 
 	void startServer() {
-
+		Network.InitializeServer(maxClients, listenPort, NATon);
 	}
 
 	void stopServer() {
+		Network.Disconnect;
+	}
 
+	void Update() {
+		if (this.serverStarted) {
+			if (Network.peerType == NetworkPeerType.Connecting) {
+				//message stating that it is connecting
+				//spinning bar/loading icon?
+			} else {
+				//message stating that the players are connected to server
+			}
+		}
 	}
 }
