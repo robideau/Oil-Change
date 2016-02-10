ddf31bb48853278a0290ad749b3ce6070992085a
rtyoung9 <rtyoung9@iastate.edu>
2016-02-09 23:21:26 -0600

    did some gui stuff with buttons and scrolling, will work on tabs and changing scene stuff next

---
+++ Assets/Scripts/test buttons/goBack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goBack : MonoBehaviour
+{
+
+    private GameObject thisScreen;
+    public GameObject newScreen;
+
+    public void goToMain(GameObject current)
+    {
+        thisScreen = current;
+        thisScreen.SetActive(false);
+        newScreen.SetActive(true);
+
+    }
+}
---
+++ Assets/Scripts/test buttons/goScroll.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goScroll : MonoBehaviour {
+
+    public GameObject thisScreen;
+    public GameObject newScreen;
+
+    public void goToScroll()
+    {
+        thisScreen.SetActive(false);
+        newScreen.SetActive(true);
+
+    }
+}
