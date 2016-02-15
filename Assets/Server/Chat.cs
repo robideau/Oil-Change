4220e6a1a2c5d28c3d4286734f66c36f4a49a44d
Nick Bramanti <bramanti@iastate.edu>
2016-02-15 12:52:16 -0600

    beginning work on a chat system

---
+++ Assets/Server/Chat.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Chat : MonoBehaviour {
+
+	public List<string> chatLog = new List<string>();
+	private string current = string.Empty;
+	private void OnGUI () {
+		GUILayout.BeginHorizontal(GUILayout.Width (250));
+		current = GUILayout.TextField (current);
+		if (GUILayout.Button ("Send")) {
+			//TODO
+		}
+		GUILayout.EndHorizontal ();
+		foreach (string x in chatLog) {
+			GUILayout.Label (x);
+		}
+	}
+}
