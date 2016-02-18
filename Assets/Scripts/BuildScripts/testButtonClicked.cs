bde8a8125317ece227cbf3433f9b641d9b34e451
jmkirchn <jmkirchn@iastate.edu>
2016-02-17 20:29:41 -0600

    Added basic test mode, test button, basic cube prefab.

---
+++ Assets/Scripts/BuildScripts/testButtonClicked.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testButtonClicked : MonoBehaviour {
+
+	public GameObject mainCamera;
+	public GameObject carPrefab;
+	public Vector3 startingPos;
+	public Quaternion startingRot;
+
+	void Start()
+	{
+		startingPos = new Vector3(0, 2, -5);
+		startingRot = Quaternion.identity;
+	}
+
+	// Use this for initialization
+	public void TestMode () {
+		carPrefab.transform.position = startingPos;
+		carPrefab.transform.rotation = startingRot;
+		carPrefab.SetActive(true);
+		mainCamera.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("escape"))
+		{
+			carPrefab.SetActive(false);
+			mainCamera.SetActive(true);
+		}
+	}
+}
