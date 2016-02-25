cbfab939a15a3326a5013c802c4f0b847f4891b2
rtyoung9 <rtyoung9@iastate.edu>
2016-02-24 23:42:39 -0600

    can now add games through create game still need to work on details of this for server management side of things

---
+++ Assets/Scripts/GatherGameInfo.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+using System.Text.RegularExpressions;
+
+public class GatherGameInfo : MonoBehaviour {
+
+    //information to gather from
+    public Dropdown timeLimit;
+    public Dropdown placeLimit;
+    public InputField gameName;
+    public Text keys;
+
+
+    public GameObject prefab;
+    GameObject prefabclone;
+    public GameObject prefabParent;
+
+    //this is part of the solution but is good for appearence sake for now
+    public void gatherInfo_addToList()
+    {
+
+        //todo--
+        //  add the invite only option
+        //  extend for passwords
+        //  once host names are figured out change the gameobject name to host's Game
+        //todo--
+
+        //make an instance of the prefab gameobject
+        prefabclone =Instantiate(prefab);
+        prefabclone.transform.SetParent(prefabParent.transform);
+
+        playableGame reference = prefabclone.GetComponent<playableGame>();
+        //set the BuildLimit for this playableGame
+        reference.setBuildLimit(Int32.Parse(placeLimit.captionText.text));
+        //set the play time for this playableGame in seconds
+        string minTime = Regex.Match(timeLimit.captionText.text, @"\d+").Value;
+        reference.setBuildTime(Int32.Parse(minTime)*60);
+        //set the name for this game
+        reference.setName(gameName.text);
+        //break they keys text down into an array that will be deep copied into the playable game
+        string keysToAdd = keys.text;
+        char[] delim = { ',', ' ' };
+        reference.setKeys(keysToAdd.Split(delim));
+         
+
+    }
+
+
+}
---
+++ Assets/Scripts/playableGame.cs
@@ -0,0 +1,183 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class playableGame : MonoBehaviour {
+    /**
+    *
+    *name for this playable game
+    *
+    */
+    private string gameName = "";
+
+    /**
+    *
+    *name of the game creater (if any)
+    *
+    */
+    private string host = "";
+
+    /**
+    *
+    *build timelimit in seconds for the game being made
+    *
+    */
+    private int buildTime;
+
+    /**
+    *
+    *building block limit given to players for this playerable game
+    *
+    */
+    private int buildLimit;
+    
+    /**
+    *
+    *array of keywords that describe the gameplay expected for this game
+    *
+    */
+    //only 10 possible keywords for now...may change later
+    private string[] keywords = new string[10];
+
+    /**
+    *total space used by keywords array, resized if needed on adder and setter methods
+    */
+    private int keySize = 10;
+
+    //need to add array of game mods?
+    //should password go here?
+
+    /**
+    *
+    *default constructor for playableGame object, has no name or host
+    *and has buildTime set to 120 and build limit set to 30
+    *
+    */
+	public playableGame()
+    {
+        buildTime = 120;
+        buildLimit = 30;
+    }
+
+    /**
+    *
+    *makes a playable game object based on paramaters
+    *gameName = name of this game
+    *buildTime = buld time for this game
+    *buildLimit = build time limit for this game
+    *host = player name who made this match
+    *
+    */
+    public playableGame(string gameName, int buildTime, int buildLimit, string host)
+    {
+        this.gameName = gameName;
+        this.buildTime = buildTime;
+        this.buildLimit = buildLimit;
+        this.host = host;
+    }
+
+    /**
+    *set the game name
+    */
+    public void setName(string gameName)
+    {
+        this.gameName = gameName;
+    }
+
+    /**
+    *set the host name
+    */
+    public void setHost(string host)
+    {
+        this.host = host;
+    }
+
+    /**
+    *set the build time
+    */
+    public void setBuildTime(int buildTime)
+    {
+        this.buildTime = buildTime;
+    }
+
+    /**
+    *set the build time limit
+    */
+    public void setBuildLimit(int buildLimit)
+    {
+        this.buildLimit = buildLimit;
+    }
+
+    /**
+    *set the keywords to given array of strings
+    */
+    public void setKeys(string[] keys)
+    {
+        keywords = new string[keys.Length];
+
+        for(int i = 0; i < keys.Length; i++)
+        {
+            keywords[i] = keys[i];
+        }
+    }
+
+    /**
+    *add the given keyword to array of keywords
+    */
+    public void addKey(string key)
+    {
+        //maximum number of keys changed
+        if(keywords.Length == keySize)
+        {
+            keySize = keySize + keySize;
+            string[] keys = new string[keySize];
+            keywords.CopyTo(keys,0);
+            keywords = keys;
+        }
+
+        keywords[keywords.Length] = key;
+
+    }
+
+    /**
+    *get the game name
+    */
+    public string getName()
+    {
+        return gameName;
+    }
+
+    /**
+    *get the host name
+    */
+    public string getHost()
+    {
+        return host;
+    }
+
+    /**
+    *get the games build limit
+    */
+    public int getBuildLimit()
+    {
+        return buildLimit;
+    }
+
+    /**
+    *get the game build time
+    */
+    public int getBuildTime()
+    {
+        return buildTime;
+    }
+
+    /**
+    *get a deep copy of all the keywords describing the game.
+    */
+    public string[] getKeyWords()
+    {
+        string[] copy = new string[keywords.Length];
+        keywords.CopyTo(copy, 0);
+        return copy;
+    }
+}
