20c35e4db14b8282358d0bdaeff055bf8d3d2cc4
rtyoung9 <rtyoung9@iastate.edu>
2016-02-26 00:19:44 -0600

    add functionality to matchmaking still needs more though

---
+++ Assets/Scripts/main screen/modifyText.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class modifyText : MonoBehaviour {
+
+    public Text modify;
+    int line = 0;
+    int trackedLength = 0;
+    public Text error;
+
+    public void clear()
+    {
+        modify.text = "";
+        trackedLength = 0;
+        error.text = "";
+        line = 0;
+    }
+
+    public void extend(string extend)
+    {
+        if(modify.text.Length < 26)
+        {
+            line = 0;
+        }
+
+        if ((trackedLength + extend.Length) > 26)
+        {
+            modify.text += "\n";
+            trackedLength = 0;
+            line++;
+        }
+        if (line == 3)
+        {
+            error.text = "maximum keys added";
+            return;
+        }
+
+        if (trackedLength != 0)
+        {
+            modify.text = modify.text + "  " + extend;
+            trackedLength += extend.Length;
+        }
+        else
+        {
+            modify.text += extend;
+            trackedLength += extend.Length;
+        }
+    }
+
+    public void extend(Dropdown pullFrom)
+    {
+        if (modify.text.Length < 26)
+        {
+            line = 0;
+        }
+
+        if ((trackedLength + pullFrom.captionText.text.Length) > 26)
+        {
+            modify.text += "\n";
+            trackedLength = 0;
+            line++;
+        }
+        if(line == 3)
+        {
+            error.text = "maximum keys added";
+            return;
+        }
+
+        if(trackedLength != 0) { 
+            modify.text = modify.text + "  " + pullFrom.captionText.text;
+            trackedLength += pullFrom.captionText.text.Length;
+        }
+        else
+        {
+            modify.text += pullFrom.captionText.text;
+            trackedLength += pullFrom.captionText.text.Length;
+        }
+    }
+}
---
+++ Assets/Scripts/main screen/updateText.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class updateText : MonoBehaviour {
+
+    public Text gameName;
+    public Text hostName;
+    public Text keyWords;
+    public Text ping;
+
+
+    public updateText()
+    {
+
+    }
+
+    public void update(string gameN, string hostN, string[] keys, int ping)
+    {
+        gameName.text = gameN;
+        hostName.text = hostN;
+        this.ping.text = ping.ToString();
+        keyWords.text = "";
+        for(int i = 0; i < keys.Length; i++)
+        {
+            keyWords.text = keyWords.text + ", " + keys[i];
+        }
+    }
+}
