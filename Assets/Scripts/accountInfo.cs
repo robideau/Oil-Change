95785d2ed55bf981771a36a394734230342433e6
rtyoung9 <rtyoung9@iastate.edu>
2016-03-03 00:20:53 -0600

    made some classes to hold player information also tweeked some more anchors

---
+++ Assets/Scripts/accountInfo.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+*create by Ryan Young March 2
+*/
+public class accountInfo : MonoBehaviour {
+
+    private string userName;
+    private List<string> friends;
+    private int friendsCount;
+    private string email;
+    private stats playerRecord;
+
+
+
+    //added features
+    //private IP address for this account?
+
+    /**
+    *checks data base for existing player name and loads in player information
+    */
+	public accountInfo(string existingPlayer)
+    {
+        //to do check for player existence
+        //load in player friends list
+        //load in player IP?
+        //load in player email if account name given
+        //load in player account name if email given
+        //have stats loaded in aswell
+
+        //for now I will just store the player name as existing player and initialize friends list as empty
+        //and set email to default
+        userName = "player";
+        friends = new List<string>();
+        email = "@nothing";
+        friendsCount = 0;
+
+        //at this point the player is know to exist and stats will access database to get player stats
+        playerRecord = new stats(userName);
+    }
+
+    /**
+    * a new player account is made so send to data base the player name and password
+    * initialize other player info to default new player
+    */
+    public accountInfo(string newPlayerName,string newPlayerEmail, string password)
+    {
+        //set username email initialize empty friends list and new stats with 0 stats so far
+        userName = newPlayerName;
+        email = newPlayerEmail;
+        friends = new List<string>();
+        playerRecord = new stats();
+        friendsCount = 0;
+
+        //all the new information will need to be sent to data base via forwardData()
+    }
+
+    /**
+    *will update player information in the data base or create new player information if needed
+    */
+    public void forwardData()
+    {
+        //need to forward current data from database (includes all statistic information)
+
+
+    }
+
+    /**
+    *helper method for gather player data
+    */
+    private void fetchData()
+    {
+        //needs to fetch data from database
+    }
+
+    /**
+    *returns account name
+    */
+    public string getName()
+    {
+        return userName;
+    }
+
+    /**
+    *returns account email
+    */
+    public string getEmail()
+    {
+        return email;
+    }
+
+    public string[] getFriends()
+    {
+        string[] friendsArray = new string[friendsCount];
+        for(int i = 0; i < friendsCount; i++)
+        {
+            friendsArray[i] = friends[i];
+        }
+
+        return friendsArray;
+    }
+
+    public void addFriend(string Friend)
+    {
+        //extend this to check data base to ensure that the friend exists in record
+        if (Friend != null)
+        {
+            friends.Add(Friend);
+        }
+        
+    }
+
+    public stats getStats()
+    {
+        return playerRecord;
+    }
+}
---
+++ Assets/Scripts/stats.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class stats : MonoBehaviour {
+
+    private int playedGames;
+    private int wonGames;
+    private int lostGames;
+    private int rank;
+    private int xp;
+
+
+	public stats()
+    {
+        playedGames = 0;
+        wonGames = 0;
+        lostGames = 0;
+        rank = 1;
+        xp = 0;
+    }
+
+    public stats(string playerName)
+    {
+        //statistics will need to be loaded from data base
+
+    }
+
+    public void completeMatch(int winStatus, int xpEarned)
+    {
+
+        //winstatus > 0 is a win < 0 is a loss == 0 is a tie
+        playedGames++;
+        if(winStatus >= 1)
+        {
+            wonGames++;
+        }
+        else if(winStatus <= -1)
+        {
+            lostGames++;
+        }
+
+        //may consider losing xp for lost games and using rank to match players in ranked matches
+        xp += xpEarned;
+
+        //some algorithm in deciding a level up in rank
+
+
+    }
+
+    /**
+    *returns the number of games won
+    */
+    public int getWins()
+    {
+        return wonGames;
+    }
+
+    /**
+    *returns the number of games lost
+    */
+    public int getLosses()
+    {
+        return lostGames;
+    }
+
+    /**
+    *returns the number of games played
+    */
+    public int getPlayed()
+    {
+        return playedGames;
+    }
+
+    /**
+    *returns player rank
+    */
+    public int getRank()
+    {
+        return rank;
+    }
+
+    /**
+    *returns player current rank xp
+    */
+    public int getXp()
+    {
+        return xp;
+    }
+
+    /**
+    *returns current needed xp for next rank
+    */
+    public int getRankXp()
+    {
+        return 0;
+    }
+}
