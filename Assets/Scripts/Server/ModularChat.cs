dd3cc1bef328bf6b8ada1e810554c7eebadb3f52
David Robideau <robideau@iastate.edu>
2016-03-07 17:44:10 -0600

    Added new chat window prefab
    Drag and drop "Chat" prefab into editor, parent to Canvas. Needs a
    network connection to function. To output a full chat log with
    timestamps, call LogString() from ModularChat.cs.

---
+++ Assets/Scripts/Server/ModularChat.cs
@@ -0,0 +1,113 @@
+﻿/* David Robideau
+ * 3/7/2016
+ * 
+ * This script implements player chat in a way that can be utilized in both build and play mode.
+ * Will leave as open-ended as possible for future use.
+ *
+ * Last update - 3/7/2016
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ModularChat : MonoBehaviour {
+
+	//UI components
+	public GameObject ChatUI; //Holds all UI components - used to easily hide/display chat
+	public UnityEngine.UI.InputField chatInput;
+	public ScrollRect chatLogView;
+	public Text chatLogText;
+	public Button sendButton;
+	public NetworkView nv;
+
+	//Message data
+	private string senderID = "Unknown";
+	private List<chatMessage> messageLog;
+
+	//Reference to player car - build mode only
+	public PlayerCarController playerCarController = null;
+
+	void Start () {
+		messageLog = new List<chatMessage> ();
+		chatLogView.transform.FindChild ("Scrollbar Vertical").GetComponent<Scrollbar> ().value = .98f;
+	}
+
+	void Update () {
+
+		//Deactivate car controls if chat input field is focused - does nothing in other modes
+		if (chatInput.isFocused && playerCarController != null) {
+			playerCarController.setMovementEnabled (false);
+			playerCarController.cameraControlEnabled = false;
+			playerCarController.fullResetEnabled = false;
+			playerCarController.shortResetEnabled = false;
+		}	else if (!chatInput.isFocused && playerCarController != null) {
+			playerCarController.setMovementEnabled (true);
+			playerCarController.cameraControlEnabled = true;
+			playerCarController.fullResetEnabled = true;
+			playerCarController.shortResetEnabled = true;
+		}
+
+		//Send message using enter key
+		if (chatInput.isFocused && Input.GetKeyDown ("return")) {
+			OnSendClick ();
+		}
+
+		//Toggle chat visible/hidden
+		if (Input.GetKeyDown ("t") && !chatInput.isFocused) {
+			ChatUI.SetActive (!ChatUI.activeSelf);
+		}
+			
+	}
+
+	//Add message to message log, send message to chat window, clear input field - called when "send" button is clicked or enter key is pressed
+	public void OnSendClick() {
+		assignSenderIDs ();
+		nv.RPC ("sendChatMessage", RPCMode.All, chatInput.text, senderID);
+		chatInput.text = "";
+		chatInput.ActivateInputField();
+	}
+
+	//Called by On End Edit method of input field - must ensure that enter key has been pressed before sending message
+	public void OnEnterPress() {
+		if (Input.GetKeyDown("return")) {
+			OnSendClick();
+		}
+	}
+
+	//Assign sender IDs - replace with actual user data when ready
+	private void assignSenderIDs() {
+		if (nv.isMine) {
+			senderID = "Player A";
+		} else {
+			senderID = "Player B";
+		}
+	}
+
+	//RPC to update chat log
+	[RPC] void sendChatMessage(string message, string senderID) {
+		chatMessage sentMessage = new chatMessage (DateTime.Now.ToShortTimeString(), senderID, message); 
+		messageLog.Add (sentMessage);
+		chatLogText.text += ("\n" + senderID + ": " + message);
+	}
+}
+
+
+//This class holds message data to be used in the detailed chat log
+public class chatMessage {
+	private string timeStamp;
+	private string senderID;
+	private string message;
+
+	public chatMessage(string timeStamp, string senderID, string message) {
+		this.timeStamp = timeStamp;
+		this.senderID = senderID;
+		this.message = message;
+	}
+
+	public string logString() {
+		return ("[" + timeStamp + "] " + senderID + ": '" + message + "'");
+	}
+}
