1510e238d22753064902507d6a2900dcb9737b16
rtyoung9 <rtyoung9@iastate.edu>
2016-03-08 23:56:45 -0600

    modified the playable game prefab. Will need further modifications in the future for passwords and such. also added some sorting capablities still need to update the display for matchmaking after sorting

--- Assets/Scripts/main screen/selectionAdd_ClearList.cs
+++ Assets/Scripts/main screen/selectionAdd_ClearList.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 using UnityEngine.UI;
 
-
 /**
-created by Ryan Young
+*renamde modify text to this Ryan Young
+*this class is used to add to a list based on words slected on a dropdown
+*
+*used as of now in the matchmaking screen for game rules and for keywords.
+*
 */
-public class modifyText : MonoBehaviour {
+public class selectionAdd_ClearList : MonoBehaviour {
 
     public Text modify;
     int line = 0;
@@ -31,12 +34,12 @@ public class modifyText : MonoBehaviour {
     */
     public void extend(string extend)
     {
-        if(modify.text.Length < 26)
+        if (modify.text.Length < 20)
         {
             line = 0;
         }
 
-        if ((trackedLength + extend.Length) > 26)
+        if ((trackedLength + extend.Length) > 20)
         {
             modify.text += "\n";
             trackedLength = 0;
@@ -65,24 +68,25 @@ public class modifyText : MonoBehaviour {
     */
     public void extend(Dropdown pullFrom)
     {
-        if (modify.text.Length < 26)
+        if (modify.text.Length < 20)
         {
             line = 0;
         }
 
-        if ((trackedLength + pullFrom.captionText.text.Length) > 26)
+        if ((trackedLength + pullFrom.captionText.text.Length) > 20)
         {
             modify.text += "\n";
             trackedLength = 0;
             line++;
         }
-        if(line == 3)
+        if (line == 3)
         {
             error.text = "maximum keys added";
             return;
         }
 
-        if(trackedLength != 0) { 
+        if (trackedLength != 0)
+        {
             modify.text = modify.text + "  " + pullFrom.captionText.text;
             trackedLength += pullFrom.captionText.text.Length;
         }
