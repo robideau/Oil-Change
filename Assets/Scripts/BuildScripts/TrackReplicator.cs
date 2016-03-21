d1c686f48e70b15fd8c96162841679c439280242
David Robideau <robideau@iastate.edu>
2016-03-21 18:49:07 -0500

    Added ability to scan and process track data
    TO SCAN FINISHED TRACK FROM BUILD MODE:
    1) call scanLevelData() (fron TrackScanner.cs)
    2) call cleanObjectNames() (from TrackScanner.cs)
    3) get string from getScannedLevelData() (from TrackScanner.cs)
    
    TO REPLICATE TRACK:
    1) place TrackReplicator prefab in desired scene
    2) call replicateTrack() (from TrackReplicator.cs) using string from
    getScannedLevelData()
    
    Only missing link is sending the string between players.

---
+++ Assets/Scripts/BuildScripts/TrackReplicator.cs
@@ -0,0 +1,41 @@
+﻿/* David Robideau
+ * 3/21/2016
+ * 
+ * This script replicates the player's opponent's track after build mode is completed.
+ * Processes level data string and instantiates prefabs in correct locations.
+ *
+ * Last update - 3/21/2016
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class TrackReplicator : MonoBehaviour {
+
+	private char delimiter = '\n';
+	private string prefabsDirectory = "BuildObjects/Test";
+	private GameObject replicatedObject = null;
+
+	void Start() {
+		replicateTrack ("ReferenceObject\n(0.0, 0.0, -20.0)\n(10, 0.05, 10)\n(0.0, 0.0, 0.0, 1.0)\n");
+	}
+
+	public void replicateTrack(string scannedTrackData) {
+		string[] trackDataLines = scannedTrackData.Split (delimiter);
+		for (int i = 0; i < trackDataLines.Length-1; i++) {
+			if (i % 4 == 0) { //Object name
+				GameObject toInstantiate = (GameObject) Resources.Load(prefabsDirectory + "/" + trackDataLines[i].Trim(), typeof(GameObject));
+				replicatedObject = Instantiate (toInstantiate);
+			} else if (i % 3 == 0) { //Rotation
+				string[] rotationData = (trackDataLines[i].Substring(1, trackDataLines[i].Length-2)).Split(',');
+				replicatedObject.transform.rotation = new Quaternion (float.Parse (rotationData [0]), float.Parse (rotationData [1]), float.Parse (rotationData [2]), float.Parse (rotationData [3]));
+			} else if (i % 2 == 0) { //Scale (lossy)
+				string[] scaleData = (trackDataLines[i].Substring(1, trackDataLines[i].Length-2)).Split(',');
+				replicatedObject.transform.localScale = new Vector3 (float.Parse (scaleData [0]), float.Parse (scaleData [1]), float.Parse (scaleData [2]));
+			} else { //Position
+				string[] positionData = (trackDataLines[i].Substring(1, trackDataLines[i].Length-2)).Split(',');
+				replicatedObject.transform.position = new Vector3 (float.Parse (positionData [0]), float.Parse (positionData [1]), float.Parse (positionData [2]));
+			}
+		}
+	}
+}
--- Assets/Scripts/BuildScripts/TrackScanner.cs
+++ Assets/Scripts/BuildScripts/TrackScanner.cs
@@ -1,9 +1,15 @@
 ﻿/* David Robideau
  * 3/9/2016
  * 
- * This script handles the processing and storage of build mode data
+ * This script handles the processing and storage of build mode data.
+ * Returns track data as a string -
+ * 		Each object has 4 lines of data:
+ * 				1: name
+ * 				2: position
+ * 				3: scale
+ * 				4: rotation
  *
- * Last update - 3/9/2016
+ * Last update - 3/21/2016
  */
 
 using UnityEngine;
@@ -34,6 +40,13 @@ public class TrackScanner : MonoBehaviour {
 	//Whether or not trackscanner is currently scanning
 	private bool scanning = false;
 
+	//Scan delay - small delay to be inserted after each scan iteration to allow collisions to register
+	public float scanDelay;
+
+	//This string holds the scanned level data to be sent to opponent
+	private string scannedLevelData = "";
+	private char delimiter = '\n';
+
 	void Start () {
 		gridCollider = worldGrid.GetComponent<BoxCollider> ();
 		gridBlockSize = objectController.gridBlockSize;
@@ -41,13 +54,11 @@ public class TrackScanner : MonoBehaviour {
 		//Add size of TrackScanner collider to compensate for overlapping colliders on build pieces
 		startX = (worldGrid.transform.position.x - (gridCollider.size.x / 2.0f)) + GetComponent<BoxCollider>().size.x;
 		startZ = (worldGrid.transform.position.y - (gridCollider.size.z / 2.0f)) + GetComponent<BoxCollider>().size.z;
-		transform.position = new Vector3 (startX, baseY + GetComponent<BoxCollider>().size.y, startZ);
+		transform.position = new Vector3 (startX, baseY, startZ);
 	}
 
 	void Update() {
-		if (Input.GetKeyDown ("q")) {
-			ProcessTrack ();
-		}
+		
 	}
 
 	//Can be called by other scripts
@@ -57,7 +68,7 @@ public class TrackScanner : MonoBehaviour {
 	 *  TrackScanner has a box collider that detects build objects. When a collision is detected, the appropriate object
 	 *  is recorded by OnTriggerEnter.
 	 */
-	public void ProcessTrack() {
+	public IEnumerator ProcessTrack() {
 		scanning = true; //begin scanning - record trigger collisions
 
 		//Iterate through all grid spaces
@@ -66,28 +77,73 @@ public class TrackScanner : MonoBehaviour {
 			for (int j = 0; j < gridZSpaces; j++) {
 				transform.position = new Vector3(startX, transform.position.y, transform.position.z);
 				for (int k = 0; k < gridXSpaces; k++) {
-					transform.position = new Vector3(startX + gridBlockSize * k, transform.position.y, transform.position.z);
-					print (transform.position);
+					transform.position = new Vector3 (startX + gridBlockSize * k, transform.position.y, transform.position.z);
+					yield return new WaitForSeconds (scanDelay);
 				}
-				transform.position = new Vector3(transform.position.x, transform.position.y, startZ + gridBlockSize * j);
+				transform.position = new Vector3 (transform.position.x, transform.position.y, startZ + gridBlockSize * j);
+				yield return new WaitForSeconds (scanDelay);
 			}
-			transform.position = new Vector3(transform.position.x, transform.position.y + yIncrements * i, transform.position.z);
+			transform.position = new Vector3 (transform.position.x, transform.position.y + yIncrements * (i+1), transform.position.z);
+			yield return new WaitForSeconds (scanDelay);
 		}
 			
 		scanning = false;
+		print ("Scan complete.");
+		yield return null;
+	}
+
+	//A (much) quicker version of ProcessTrack() - to be used as default unless we start running into issues.
+	public IEnumerator QuickProcessTrack() {
+		scanning = true;
+		GetComponent<BoxCollider> ().size = new Vector3 (gridXSpaces * gridBlockSize, 1, gridZSpaces * gridBlockSize);
+		transform.position = new Vector3 (0, baseY - 1, 0);
+		while (transform.position.y < yIncrements * gridYSpaces) {
+			transform.position = new Vector3 (0, transform.position.y + 1, 0);
+			yield return new WaitForSeconds (scanDelay);
+		}
+		scanning = false;
+		print ("Quick scan complete.");
+		yield return null;
 	}
 
 	void OnTriggerEnter(Collider other) {
 		//If other object is a placed track piece
 		if (scanning) {
 			if (other.gameObject.tag == "BuildObject" || other.gameObject.tag == "Finish" || other.gameObject.tag == "Start") {
-				
+				scannedLevelData += other.gameObject.name + "\n";
+				scannedLevelData += other.transform.position + "\n";
+				scannedLevelData += "(" + other.transform.lossyScale.x + ", " + other.transform.lossyScale.y + ", " + other.transform.lossyScale.z + ")" + "\n";
+				scannedLevelData += other.transform.rotation + "\n";
+			}
+		}
+	}
+
+	//Clean object names in scanned level data
+	//Use this function to remove (#) tags on the end of prefab instances
+	//Trims strings at first space - VERY IMPORTANT: DO NOT USE SPACES IN PREFAB NAMES
+	private void cleanObjectNames() {
+		string[] stringLines = scannedLevelData.Split (delimiter);
+		scannedLevelData = "";
+		for (int i = 0; i < stringLines.Length; i++) {
+			if (i % 4 == 0) {
+				string[] tokens = stringLines [i].Split (' ');
+				scannedLevelData += (tokens [0] + "\n");
+			} else {
+				scannedLevelData += (stringLines [i] + "\n");
 			}
 		}
 	}
 
 	public bool IsScanning() {
 		return scanning;
+	}
+
+	public void scanLevelData() {
+		StartCoroutine (QuickProcessTrack ());
+	}
+
+	public string getScannedLevelData() {
+		return scannedLevelData;
 	}
 		
 }
