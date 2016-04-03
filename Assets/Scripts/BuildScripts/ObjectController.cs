10ea8057f17aedaa5b4a609a2a960f8dde9514d9
jmkirchn <jmkirchn@iastate.edu>
2016-04-03 11:08:27 -0500

    - Disabled building while in Test Mode

--- Assets/Scripts/BuildScripts/ObjectController.cs
+++ Assets/Scripts/BuildScripts/ObjectController.cs
@@ -21,6 +21,7 @@ public class ObjectController : MonoBehaviour {
 	public Text buildLimitWarning;
 	public Text buildLimitCounter;
 	public ModularChat chat;
+	public testButtonClicked tbscript;
 
 	private GameObject currentObject;
 	private bool buildMenuTestDir;
@@ -46,69 +47,77 @@ public class ObjectController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		// Place object
-		if(Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+		if(!tbscript.getTestMode())
 		{
-			currentObject = null;
-			newPiecePlaced = true;
-			updateBuildCounterText ();
-			chat.enableInput ();
-		}
-		// Delete object
-		if((Input.GetMouseButtonDown(2) || Input.GetKey("delete")) && !EventSystem.current.IsPointerOverGameObject())
-		{
-			RaycastHit hit;
-			if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, rayCastDist))
+			// Place object
+			if(Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
 			{
-				string hitTag = hit.collider.gameObject.tag;
-				if (hitTag == "BuildObject" || hitTag == "Finish" || hitTag == "Start") {
-					Destroy(hit.collider.gameObject);
-					buildCount--;
-					updateBuildCounterText ();
+				currentObject = null;
+				newPiecePlaced = true;
+				updateBuildCounterText ();
+				chat.enableInput ();
+			}
+			// Delete object
+			if((Input.GetMouseButtonDown(2) || Input.GetKey("delete")) && !EventSystem.current.IsPointerOverGameObject())
+			{
+				RaycastHit hit;
+				if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, rayCastDist))
+				{
+					string hitTag = hit.collider.gameObject.tag;
+					if (hitTag == "BuildObject" || hitTag == "Finish" || hitTag == "Start") {
+						Destroy(hit.collider.gameObject);
+						buildCount--;
+						updateBuildCounterText ();
+					}
 				}
 			}
-		}
-		if(Input.GetKeyDown("page up"))
-		{
-			worldGrid.transform.position = new Vector3(0.0f, worldGrid.transform.position.y + 5.0f, 0.0f);
-			Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, 
-														 Camera.main.transform.position.y + 5.0f, 
-														 Camera.main.transform.position.z);
-			// "Refresh" currentObject
-			currentObject.SetActive(false);
-			currentObject.SetActive(true);
-		}
-		if(Input.GetKeyDown("page down"))
-		{
-			worldGrid.transform.position = new Vector3(0.0f, worldGrid.transform.position.y - 5.0f, 0.0f);
-			Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, 
-														 Camera.main.transform.position.y - 5.0f, 
-														 Camera.main.transform.position.z);
-			// "Refresh" currentObject
-			currentObject.SetActive(false);
-			currentObject.SetActive(true);
-		}
+			if(Input.GetKeyDown("page up"))
+			{
+				worldGrid.transform.position = new Vector3(0.0f, worldGrid.transform.position.y + 5.0f, 0.0f);
+				Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, 
+															 Camera.main.transform.position.y + 5.0f, 
+															 Camera.main.transform.position.z);
+				// "Refresh" currentObject
+				currentObject.SetActive(false);
+				currentObject.SetActive(true);
+			}
+			if(Input.GetKeyDown("page down"))
+			{
+				worldGrid.transform.position = new Vector3(0.0f, worldGrid.transform.position.y - 5.0f, 0.0f);
+				Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, 
+															 Camera.main.transform.position.y - 5.0f, 
+															 Camera.main.transform.position.z);
+				// "Refresh" currentObject
+				currentObject.SetActive(false);
+				currentObject.SetActive(true);
+			}
 
-		// Move currentObject with mouse pointer
-		if(currentObject)
-		{
-			RaycastHit hit;
-			if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, rayCastDist))
+			// Move currentObject with mouse pointer
+			if(currentObject)
 			{
-				if(!currentObject.activeSelf)
-					currentObject.SetActive(true);
-				if (hit.collider.gameObject.tag == "Grid") {
-					hitPoint = hit.point;
-					hitPoint.x = Mathf.Round(hit.point.x / gridBlockSize) * gridBlockSize; //Snap X
-					hitPoint.z = Mathf.Round(hit.point.z / gridBlockSize) * gridBlockSize; //Snap Z
-					currentObject.transform.position = hitPoint; //Snap to grid
+				RaycastHit hit;
+				if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, rayCastDist))
+				{
+					if(!currentObject.activeSelf)
+						currentObject.SetActive(true);
+					if (hit.collider.gameObject.tag == "Grid") {
+						hitPoint = hit.point;
+						hitPoint.x = Mathf.Round(hit.point.x / gridBlockSize) * gridBlockSize; //Snap X
+						hitPoint.z = Mathf.Round(hit.point.z / gridBlockSize) * gridBlockSize; //Snap Z
+						currentObject.transform.position = hitPoint; //Snap to grid
+					}
+				}
+				else
+				{
+					if(currentObject.activeSelf)
+						currentObject.SetActive(false);
 				}
 			}
-			else
-			{
-				if(currentObject.activeSelf)
-					currentObject.SetActive(false);
-			}
+		}
+		else
+		{
+			if(currentObject.activeSelf)
+				currentObject.SetActive(false);
 		}
 	}
 
