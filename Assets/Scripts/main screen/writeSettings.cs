5c774462888f87ad0446f7954e4854508cc48954
Nick Bramanti <bramanti@iastate.edu>
2016-04-12 13:59:47 -0500

    Writing a file after setting changes and having those changes implemented immediately

--- Assets/Scripts/main screen/writeSettings.cs
+++ Assets/Scripts/main screen/writeSettings.cs
@@ -8,92 +8,112 @@ using UnityEngine;
 using System.Collections;
 using System.IO;
 using UnityEngine.UI;
+using System;
+using System.Text;
 
 public class writeSettings : MonoBehaviour {
 
 	public string filePath = "settings.txt";
 	public FileStream file;
-	public string displayText = "Option A";
-	public string resolutionText = "Option A";
-	public string qualityText = "Option A";
-	public string audioText = "Option A";
+	public string displayText = "Full Screen";
+	public string resolutionText = "1920*1080";
+	public string qualityText = "Low";
+	public string audioText = "";
 	public Dropdown displayDrop;
 	public Dropdown resolutionDrop;
 	public Dropdown qualityDrop;
 	public Dropdown audioDrop;
+	public bool fullScreen = false;
 
 	void Start() {
-		//new FileStream with OpenOrCreate mode which opens the file if it is there
-		//or creates the file if it is not
-		file = new FileStream (filePath, FileMode.OpenOrCreate);
+		//check for file if its there read and write in settings or else create the file
 	}
 
 	//a function that will be called when display is changed
 	public void updateDisplay() {
 		switch (displayDrop.value) {
-			case 0:
-				displayText = "Option A";
-				break;
-			case 1:
-				displayText = "Option B";
-				break;
-			case 2:
-				displayText = "Option C";
-				break;
+		case 0:
+			displayText = "Full Screen";
+			fullScreen = true;
+			break;
+		case 1:
+			displayText = "Windowed";
+			Screen.fullScreen = false;
+			break;
 		}
-		updateFile (0);
 	}
 
 	//a function that will be called when resolution is changed
 	public void updateResolution() {
 		switch (resolutionDrop.value) {
 		case 0:
-			resolutionText = "Option A";
+			resolutionText = "1920*1080";
+			Screen.SetResolution (1920, 1080, fullScreen, 0);
 			break;
 		case 1:
-			resolutionText = "Option B";
+			resolutionText = "1920*1200";
+			Screen.SetResolution (1920, 1200, fullScreen, 0);
 			break;
 		case 2:
-			resolutionText = "Option C";
+			resolutionText = "1600*1200";
+			Screen.SetResolution (1600, 1200, fullScreen, 0);
 			break;
 		}
-		updateFile (1);
 	}
 
 	//a function that will be called when quality is changed
 	public void updateQuality() {
 		switch (qualityDrop.value) {
 		case 0:
-			qualityText = "Option A";
+			qualityText = "Low";
+			QualitySettings.SetQualityLevel (0);
 			break;
 		case 1:
-			qualityText = "Option B";
+			qualityText = "Medium";
+			QualitySettings.SetQualityLevel (3);
 			break;
 		case 2:
-			qualityText = "Option C";
+			qualityText = "High";
+			QualitySettings.SetQualityLevel (5);
 			break;
 		}
-		updateFile (2);
 	}
 
 	//a function that will be called when audio is changed
 	public void updateAudio() {
 		switch (audioDrop.value) {
 		case 0:
-			audioText = "Option A";
+			audioText = "Off";
+			AudioListener.volume = 0;
 			break;
 		case 1:
-			audioText = "Option B";
+			audioText = "Low";
+			AudioListener.volume = 0.25F;
 			break;
 		case 2:
-			audioText = "Option C";
+			audioText = "Medium";
+			AudioListener.volume = 0.5F;
+			break;
+		case 3:
+			audioText = "High";
+			AudioListener.volume = 0.75F;
+			break;
+		case 4:
+			audioText = "Full";
+			AudioListener.volume = 1;
 			break;
 		}
-		updateFile (3);
 	}
 
-	//called after each change to update the settings file
-	public void updateFile (int i) {
-		
+	//called after exiting to update the settings file
+	public void updateFile () {
+		Debug.Log ("updateFile called");
+		//make the string which is 
+		string s = "Display: " + displayText + "\n";
+		s += "Resolution: " + resolutionText + "\n";
+		s += "Quality: " + qualityText + "\n";
+		s += "Audio: " + audioText;
+		Debug.Log (s);
+		File.WriteAllText ("settings.txt", s);
 	}
 }
