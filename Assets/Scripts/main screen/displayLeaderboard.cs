de618d03487319b04ad40fbeb3b4ee631d078494
jmkirchn <jmkirchn@iastate.edu>
2016-04-18 18:44:38 -0500

    - Leaderboard is now fetched from database   -> when entering leaderboard window

---
+++ Assets/Scripts/main screen/displayLeaderboard.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class displayLeaderboard : MonoBehaviour {
+
+	private bool updating;
+	private string[] leaderboard;
+
+	public void getLeaderboard()
+	{
+		updating = false;
+		Debug.Log("updating leaderboard");
+		StartCoroutine(updateRankings());
+		Debug.Log("Leaderboard updated");
+		StartCoroutine(fetchLeaderboard());
+	}
+
+	//updates leaderboard in database
+	private IEnumerator updateRankings()
+	{
+		updating = true;
+		string post_url = "http://proj-309-38.cs.iastate.edu/php/updaterankings.php?";
+		WWW u_check = new WWW(post_url);
+		yield return u_check;
+		updating = false;
+	}
+
+	private IEnumerator fetchLeaderboard()
+	{
+		//wait for leaderboards to update
+		while (updating)
+		{
+			yield return new WaitForSeconds(0.1f);
+		}
+
+		//connect to server and gather leaderboard
+		//returned string is sorted by ranking
+		string post_url = "http://proj-309-38.cs.iastate.edu/php/getleaderboard.php";
+		WWW lb_check = new WWW(post_url);
+		yield return lb_check;
+		leaderboard = lb_check.text.Split('\n');
+	}
+
+}
