39e114b0ee4ca12f09f4690722003c3476e6dfb2
jmkirchn <jmkirchn@iastate.edu>
2016-04-20 16:37:31 -0500

    - Basic song rotation script added - Added Audio Folder and 1 song
    Need to work on audio choosing correct 'scene music' when switching scenes. Also have not tested song switching yet.

---
+++ Assets/musicControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class musicControl : MonoBehaviour {
+
+	public GameObject menuMusic;
+	public GameObject buildMusic;
+	public GameObject raceMusic;
+	public GameObject currentFolder;
+	private GameObject currentSong;
+
+
+	// Use this for initialization
+	void Start () {
+		int menuSongCount = menuMusic.transform.childCount;
+
+		currentFolder = menuMusic;
+
+		currentSong = menuMusic.transform.GetChild(Random.Range(0, menuSongCount)).gameObject;
+
+		currentSong.SetActive(true);
+
+		Invoke("changeSong", currentSong.GetComponent<AudioSource>().clip.length);
+	}
+
+	private void changeSong ()
+	{
+		currentSong.SetActive(false);
+		int folderSongCount = currentFolder.transform.childCount;
+		currentSong = menuMusic.transform.GetChild(Random.Range(0, folderSongCount)).gameObject;
+		currentSong.SetActive(true);
+	}
+}
